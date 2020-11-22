a3e423923db81a4b27aa4229ef40751ebfc6fe80
אליעזר הורוביץ <eli191191@outlook.co.il>
2020-11-22 03:31:24 +0200

    exe3

---
+++ dotNet8781_03_9870/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet8781_03_9870
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
